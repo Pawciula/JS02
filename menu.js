c1e265b44fb9633d8e905a26afe8dad4573538e9
Pawciula <127253884+Pawciula@users.noreply.github.com>
2023-10-03 09:27:50 +0200

    Add files via upload

---
+++ menu.js
@@ -0,0 +1,5 @@
+document.write('<a href="index.html">Strona główna</a>');
+document.write('<a href="if_else.html">Instrukcja if...else</a>');
+document.write('<a href="if_elseif_else.html">Zagnieżdżona instrukcja if...else</a>');
+document.write('<a href="operator_warunkowy.html">Operator warunkowy</a>');
+document.write('<a href="switch.html">Instrukcja switch</a>');
---
+++ zad4.js
@@ -0,0 +1,4 @@
+let x = 11;
+			
+let jaka = (x%2 == 0) ? "parzysta":"nie parzysta";
+document.write(`${x} ${jaka}`);
